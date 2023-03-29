6c8a57c3a7462b5a5ce98fe16cf881350fe6bbe1
Tanner Ensign <81951584+tensign1444@users.noreply.github.com>
2023-03-28 19:49:47 -0700

    Add files via upload

--- Include/MemoryManager.h
+++ Include/MemoryManager.h
@@ -9,6 +9,7 @@
 #ifndef MEMORYMANAGER_MEMORYMANAGER_H
 #define MEMORYMANAGER_MEMORYMANAGER_H
 
+
 #include "../DataStructures/Include/LinkedList.h"
 #include "../DataStructures/Include/Queue.h"
 
@@ -41,6 +42,76 @@ NODE *createNode(size_t size, bool free){
     data->size = size;
     return data;
 }
+
+/**
+ * Initalize a new memory manager object.
+ * @param maxSize the maximum size of memory for the memory manager to use.
+ * @return pointer to the memory management list.
+ */
+static LIST *initMemory(size_t maxSize);
+
+/**
+ * Changes to node isFree bool to true, and then coalesces the previous and before nodes to
+ * @param start node to begin the loop at.
+ */
+static void freeNode(NODE* start);
+
+/**
+ * Frees the memory manager from the windows memory manager.
+ */
+static void freeMemory();
+
+/**
+ * Changes to node isFree bool to true, and then coalesces the previous and before nodes to
+ * @param start node to begin the loop at.
+ */
+static void freeNode(NODE* start);
+
+/**
+* Finds the first free block of memory in the linked list that is the same or larger amount of size we need.
+* @param start node to begin the loop at.
+* @param size of space we need.
+* @return the node with enough space.
+*/
+static NODE *findFree(NODE *start, size_t size, bool found);
+
+
+/**
+* Splits a page of memory if the size is larger than what is needed.
+* @param page to split.
+* @param amount of memory needed.
+* @return new page;
+*/
+static NODE *splitPage(NODE *page, size_t amount);
+
+
+/**
+ * Helper method that preforms a best fit memory management algorithm in our memory manager.
+ * @param amount amount of bytes to allocate.
+ * @return a pointer to the memory location allocated.
+ */
+static NODE *bestFit(size_t amount);
+
+/**
+* Request memory from the memory manager linked list created when initializing the linked list. This
+* does not use Malloc. Instead Malloc is used in initializing the memory manager, so we only use it once and split
+* that memory up.
+* The user can use best fit or first fit. In order to use best fit, the BEST_FIT macro at the top of the source file must be
+* set to 1.
+* @param amount of memory needed.
+* @return pointer to the new node with the size requested.
+*/
+static NODE *requestMemory(size_t amount);
+
+
+/**
+ * Allocates memory inside the memory manager created with the intialization function.
+ * @param size of memory to allocate
+ * @return pointer to the memory location of the allocation.
+ */
+NODE *Malloc(size_t size);
+
+void DumpMemoryList();
 
 
 
