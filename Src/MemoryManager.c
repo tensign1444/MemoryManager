3a2d39cb69f545d6423b086c4b4fb4a600af1dc5
Tanner Ensign <81951584+tensign1444@users.noreply.github.com>
2023-04-04 17:14:34 -0700

    Update MemoryManager.c
    Fixed free memory location

--- Src/MemoryManager.c
+++ Src/MemoryManager.c
@@ -1,6 +1,7 @@
 #include "../Include/MemoryManager.h"
 #include <stdlib.h>
 #include "../DataStructures/Include/Utility.h"
+#include "../DataStructures/Include/LinkedList.h"
 /**
  * @file MemoryManager.c
  *
@@ -15,14 +16,27 @@
 /**
  * The memory manager created.
  */
-static LIST *manager;
+LIST *manager;
 
 /**
- * Creates a new memory manager structure by initializing the busy list, free list, memory list, and queue. Sets
+ * Creates a node for the user when prompted.
+ * @param size amount of memory.
+ * @param free is the node free of data.
+ * @return pointer to node.
+ */
+NODE *createNode(size_t size, bool Free) {
+    NODE *data;
+    data->isFree = Free;
+    data->size = size;
+    return data;
+}
+
+/**
+ * Creates a new memory manager structure by initiliazing the busy list, free list, memory list, and queue. Sets
  * the count equal to 0.
  * @return MEMORY structure pointer.
  */
-static LIST *initMemory(size_t maxSize){
+LIST *initMemory(size_t maxSize){
     size_t required = maxSize;
     while(manager == NULL){
         manager = InitList(compare_int64_t);
@@ -45,80 +59,90 @@ static LIST *initMemory(size_t maxSize){
  * Changes to node isFree bool to true, and then coalesces the previous and before nodes to
  * @param start node to begin the loop at.
  */
-static void freeNode(NODE* start) {
-    NODE *curr = start;
-    start->isFree = true;
-    while (curr->next != NULL) {
-        if (curr->isFree) {
-            if (curr->next->isFree) {
-                curr->size += curr->next->size;
-                free(curr->next);
-            }
-            if (curr->previous->isFree) {
-                curr->size += curr->previous->size;
-                free(curr);
-            }
-            curr = curr->next;
-        }
+int freeMemoryLocation(LIST* Mem, size_t start) {
+    int indexOfNode = IndexOfValue(Mem, &start);
+    NODE *curr = Mem->head;
+    for(int i = 0; i < indexOfNode; i++){ curr = curr->next}
 
+    if(curr->value == &start){
+        if(curr->isFree){
+            printf("ERROR: Node is already free!");
+            return EXIT_FAILURE;
+        }else{
+            if(curr->previous->isFree){
+                size_t sum = (size_t) curr->previous->value + (size_t) curr->value;
+                curr->previous->value = &sum;
+                curr = curr->previous;
+                UnlinkNodeByValue(Mem, curr->next->value);
+            }
+            if(curr->next->isFree){
+                size_t sum = (size_t) curr->next->value + (size_t) curr->value;
+                curr->next->value = &sum;
+                curr = curr->next;
+                UnlinkNodeByValue(Mem, curr->previous->value);
+            }
+            return EXIT_SUCCESS;
+        }
     }
 }
 
 /**
  * Frees the memory manager from the windows memory manager.
  */
-static void freeMemory(){ free(manager);}
+void freeMemory(){ free(manager);}
+
+
 
 /**
- * Finds the first free block of memory in the linked list that is the same or larger amount of size we need.
- * @param start node to begin the loop at.
- * @param size of space we need.
- * @return the node with enough space.
- */
-static NODE *findFree(NODE *start, size_t size, bool found){
+* Finds the first free block of memory in the linked list that is the same or larger amount of size we need.
+* @param start node to begin the loop at.
+* @param size of space we need.
+* @return the node with enough space.
+*/
+NODE *findFree(NODE *start, size_t size, bool found) {
 
     NODE *curr = start;
     while (curr->next != NULL) {
-        if(curr->isFree)
-            if(curr->size >= size)
+        if (curr->isFree)
+            if (curr->size >= size)
                 return curr;
         curr = curr->next;
     }
-    if(found)
+    if (found)
         return NULL;
-    else{
+    else {
         perror("No free memory!");
         exit(0);
     }
 }
 
 
+
 /**
- * Splits a page of memory if the size is larger than what is needed.
- * @param page to split.
- * @param amount of memory needed.
- * @return new page;
- */
-static NODE *splitPage(NODE *page, size_t amount){
-    if(page->size == amount){
+* Splits a page of memory if the size is larger than what is needed.
+* @param page to split.
+* @param amount of memory needed.
+* @return new page;
+*/
+NODE *splitPage(NODE *page, size_t amount) {
+    if (page->size == amount) {
         page->isFree = false;
         return page;
-    }
-    else{
-        NODE *newPage = createNode(page->size - amount, true);
-        page->size = amount;
+    } else {
+        NODE *newPage = createNode((size_t)page->value - amount, true);
+        page->value = &amount;
         page->isFree = false;
-        InsertNodeAfterTarget(manager, IndexOf(manager, page), newPage);
+        InsertNodeAfterTarget(manager, IndexOfValue(manager, page->value), newPage);
     }
 }
 
 
-static NODE *bestFit(size_t amount){
+NODE *bestFit(size_t amount) {
     NODE *curr;
-    NODE *tempBest = findFree(manager->head);
-    while(true){
-        curr = findFree(curr);
-        if(tempBest->size - amount > curr->size - amount)
+    NODE *tempBest = findFree(manager->head, amount, false);
+    while (true) {
+        curr = findFree(curr, amount, true);
+        if (tempBest->size - amount > curr->size - amount)
             tempBest = curr;
     }
     return tempBest;
@@ -126,21 +150,21 @@ static NODE *bestFit(size_t amount){
 
 
 /**
- * Request memory from the memory manager linked list created when intializing the linked list. This
- * does not use Malloc. Instead Malloc is used in intializing the memory manager, so we only use it once and split
- * that memory up.
- * The user can use best fit or first fit. In order to use best fit, the BEST_FIT macro at the top of the source file must be
- * set to 1.
- * @param amount of memory needed.
- * @return pointer to the new node with the size requested.
- */
-static NODE *requestMemory(size_t amount){
+* Request memory from the memory manager linked list created when intializing the linked list. This
+* does not use Malloc. Instead Malloc is used in intializing the memory manager, so we only use it once and split
+* that memory up.
+* The user can use best fit or first fit. In order to use best fit, the BEST_FIT macro at the top of the source file must be
+* set to 1.
+* @param amount of memory needed.
+* @return pointer to the new node with the size requested.
+*/
+NODE *requestMemory(size_t amount) {
     NODE *temp = manager->head;
-    NODE *freePage = findFree(temp);
-    if(BEST_FIT) {
+    NODE *freePage = findFree(temp, amount, false);
+    if (BEST_FIT) {
         freePage = bestFit(amount);
         freePage = splitPage(freePage, amount);
-    }else{
+    } else {
         freePage = splitPage(freePage, amount);
         return freePage;
     }
@@ -151,6 +175,37 @@ static NODE *requestMemory(size_t amount){
  * @param size of memory to allocate
  * @return pointer to the memory location of the allocation.
  */
-NODE *Malloc(size_t size){
+NODE *Malloc(size_t size) {
     return requestMemory(size);
 }
+
+void DumpMemoryList() {
+    LIST *freeList;
+    LIST *busyList;
+    NODE *curr = manager->head;
+    while (curr->next != NULL) {
+        if (curr->isFree) {
+            Add(freeList, curr);
+        } else {
+            Add(busyList, curr);
+        }
+        curr = curr->next;
+    }
+
+    curr = freeList->head;
+    printf("Free Memory:\n");
+    while (curr->next != NULL) {
+        printf("Size: %s", curr->value);
+        curr = curr->next;
+    }
+    printf("Size: %s\n", curr->value);
+
+    curr = busyList->head;
+    printf("Busy Memory:\n");
+    while (curr->next != NULL) {
+        printf("Size: %s\n", curr->value);
+    }
+    printf("Size: %s", curr->value);
+}
+
+
